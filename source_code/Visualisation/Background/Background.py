64d50d3e456b8ed22b1f489ecc52fd431af5c349
gitpat-ea <pateshman.ea@phystech.edu>
2022-11-26 18:18:45 +0300

    added correct path and collaborate working of modules

--- source_code/Visualisation/Background/Background.py
+++ source_code/Visualisation/Background/Background.py
@@ -1,15 +1,13 @@
 import pygame as pg
 
 
-background_image = pg.image.load('Cyberpunk-2077-logo_1920x1080.jpeg')
-
-
 def background_fill(screen):
     """
     Varning!!! put in the top of the main cicle
     :param screen: screen where photo must be put
     :return: fillss background with photo
     """
+    background_image = pg.image.load('/Users/egorpatesman/PycharmProjects/RhythmGame/source_code/Visualisation/Background/Cyberpunk-2077-logo_1920x1080.jpeg')
 
     screen.blit(background_image, (0, 0))
 
--- source_code/Visualisation/Menu/Vis_Choose_a_song_Menu.py
+++ source_code/Visualisation/Menu/Vis_Choose_a_song_Menu.py
@@ -23,47 +23,47 @@ class VisualisationInChooseSongMenu:
 
     def draw_easy_button(self):
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        easy = font.render('easy', 1, green, black)
+        easy = font.render('easy', 1, green)
         self.surface.blit(easy, (580, 100))
 
     def draw_medium_button(self):
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        medium = font.render('medium', 1, yellow, black)
+        medium = font.render('medium', 1, yellow)
         self.surface.blit(medium, (580-20, 300))
 
     def draw_hard_button(self):
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        hard = font.render('hard', 1, red, black)
+        hard = font.render('hard', 1, red)
         self.surface.blit(hard, (580, 500))
 
     def draw_easy_song_names(self):
         # FIXME: add song names
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        first_easy_song = font.render('name1', 1, white, black)
+        first_easy_song = font.render('name1', 1, white)
         self.surface.blit(first_easy_song, (367-x, 200))
-        second_easy_song = font.render('name2', 1, white, black)
+        second_easy_song = font.render('name2', 1, white)
         self.surface.blit(second_easy_song, (580-x, 200))
-        third_easy_song = font.render('name3', 1, white, black)
+        third_easy_song = font.render('name3', 1, white)
         self.surface.blit(third_easy_song, (793-x, 200))
 
     def draw_medium_song_names(self):
         # FIXME: add song names
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        first_medium_song = font.render('name1', 1, white, black)
+        first_medium_song = font.render('name1', 1, white)
         self.surface.blit(first_medium_song, (367-x, 400))
-        second_medium_song = font.render('name2', 1, white, black)
+        second_medium_song = font.render('name2', 1, white)
         self.surface.blit(second_medium_song, (580-x, 400))
-        third_medium_song = font.render('name3', 1, white, black)
+        third_medium_song = font.render('name3', 1, white)
         self.surface.blit(third_medium_song, (793-x, 400))
 
     def draw_hard_song_names(self):
         # FIXME: add song names
         font = pg.font.Font('Sunset Club Free Trial.ttf', 32)
-        first_hard_song = font.render('name1', 1, white, black)
+        first_hard_song = font.render('name1', 1, white)
         self.surface.blit(first_hard_song, (367-x, 600))
-        second_hard_song = font.render('name2', 1, white, black)
+        second_hard_song = font.render('name2', 1, white)
         self.surface.blit(second_hard_song, (580-x, 600))
-        third_hard_song = font.render('name3', 1, white, black)
+        third_hard_song = font.render('name3', 1, white)
         self.surface.blit(third_hard_song, (793-x, 600))
 
     def all_menu_drawer(self):
