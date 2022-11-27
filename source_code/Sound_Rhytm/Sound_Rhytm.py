ff49a6eda237773ccd244416bb5607fdbc06c4fa
rin-rin16 <danikrynkevich@gmail.com>
2022-11-27 20:35:01 +0300

    Added an easy way to add new tracks to the game via mother class

--- source_code/Sound_Rhytm/Sound_Rhytm.py
+++ source_code/Sound_Rhytm/Sound_Rhytm.py
@@ -47,15 +47,25 @@ class BullVariables:
 
 class Mouse_Mode_Track_1:
     """Class of functions, which executes mouse mode gameplay for different tracks, corresponding to track number"""
+    def __init__(self, track_name):
+        """Defines the name of the track which will be played"""
+        self.track_name = track_name
+
     def number_checker(self, trek_number):
+        """
+        Activates music_player function only for corresponding track number
+        :param trek_number: track number
+        :return: True, if the number fits the track
+        """
         if trek_number == 1:
             return True
 
     def music_player(self, start_time, bpm, fase, lower_bound, upper_bound, draw_balls, balls, running, trek_number):
+        """Mother of all the music players. Plays music with certain name and does bit check with certain bpm"""
         pg.mixer.init()  # Initializing audio player
         pg.mixer.music.set_volume(0.5)
         if self.number_checker(trek_number):
-            pg.mixer.music.load("Soundtracks/Phonk/4WHEEL_-_KERAUNOS_KILLER_Speed_Up_73991451.mp3")
+            pg.mixer.music.load(self.track_name)
             pg.mixer.music.play()
             game_running = True
             while game_running:
@@ -63,8 +73,9 @@ class Mouse_Mode_Track_1:
                 SR.TimerBull.timer(start_time, bpm, fase, lower_bound, upper_bound)
                 M_Eng.Drawer(draw_balls)
                 pg.display.update()
+                M_Eng.Event_Holder("q", balls, draw_balls, SR.TimerBull)        # Commented stuff is here for testing
                 for event in pg.event.get():
-                    M_Eng.Event_Holder(event, balls, draw_balls, SR.TimerBull)
+                    #M_Eng.Event_Holder(event, balls, draw_balls, SR.TimerBull)
                     if event.type == pg.QUIT:
                         running.setter(False)
                         game_running = False
@@ -79,8 +90,8 @@ class Mouse_Mode_Track_2(Mouse_Mode_Track_1):
 start_time = NumVariables()
 TimerBull = BullVariables()
 
-Trak_1_Player = Mouse_Mode_Track_1()
-Track_2_Player = Mouse_Mode_Track_2()
+Trak_1_Player = Mouse_Mode_Track_1("Soundtracks/Phonk/4WHEEL_-_KERAUNOS_KILLER_Speed_Up_73991451.mp3")
+Track_2_Player = Mouse_Mode_Track_2("Soundtracks/Phonk/KORDHELL_-_Live_Another_Day_73349846.mp3")
 
 
 
