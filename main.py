477d602b88f24ae1fe6f97cde65079b79026a579
SavoSyka <savostikov.artem@gmail.com>
2022-11-27 03:29:36 +0300

    make the first prototype of buttons and menu

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from source_code.Sound_Rhytm import Sound_Rhytm as SR
 from source_code.Engine import Mouse_Mode as M_Eng
 from source_code.Visualisation.Game import Vis_Mouse_Mode as Ms_Vis
+from source_code.Engine.menu import start_menu
 
 #width, height = 1280, 720       # Screen's width and height        # Took this into engine file
 #background_color = (0, 0, 0)
@@ -20,16 +21,66 @@ running = True
 
 [balls, draw_balls] = M_Eng.ball_initializer()
 
-pg.mixer.music.play()
-
 while running:
-    M_Eng.screen.fill((0, 0, 0))
-    SR.TimerBull.timer(SR.start_time)
-    M_Eng.Drawer(draw_balls)
-    pg.display.update()
-    for event in pg.event.get():
-        M_Eng.Event_Holder(event, balls, draw_balls, SR.TimerBull)
-        if event.type == pg.QUIT:
-            running = False
+    menu_running = True
+    if menu_running:
+        M_Eng.screen.fill((0, 0, 0))
+        play_button = start_menu.PlayButton(100, 100, 50, 50, 'play_button')
+        quit_button = start_menu.Button(100, 200, 50, 50, 'quit_button')
+        play_button.write_text_on_button(M_Eng.screen)
+        quit_button.write_text_on_button(M_Eng.screen)
+        pg.display.update()
+
+        for event in pg.event.get():
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if play_button.is_click(event):
+                    play_button.start_game()
+                    menu_running = False
+                if quit_button.is_click(event):
+                    running = False
+            elif event.type == pg.QUIT:
+                running = False
+
+    if start_menu.trek_choice:
+        choice_running = True
+        while choice_running:
+            M_Eng.screen.fill((0, 0, 0))
+
+            amount_of_buttons = 5
+            trek_button = [0]*amount_of_buttons
+            for i in range(amount_of_buttons):
+                trek_button[i] = start_menu.TrekButton(100, i*100+100, 50, 50, f'trek_button_{i+1}', trek_number=i+1)
+                trek_button[i].write_text_on_button(M_Eng.screen)
+            back_to_menu = start_menu.Button(500,100,50,50,'Back')
+            back_to_menu.write_text_on_button(M_Eng.screen)
+            pg.display.update()
+
+            for event in pg.event.get():
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    for i in range(amount_of_buttons):
+                        if trek_button[i].is_click(event):
+                            start_menu.trek_number = i+1
+                            choice_running = False
+                    if back_to_menu.is_click(event):
+                        menu_running = True
+                        choice_running = False
+                        start_menu.trek_choice = False
+                elif event.type == pg.QUIT:
+                    choice_running = False
+                    running = False
+
+    if start_menu.trek_number == 1:
+        pg.mixer.music.play()
+        game_running = True
+        while game_running:
+            M_Eng.screen.fill((0, 0, 0))
+            SR.TimerBull.timer(SR.start_time)
+            M_Eng.Drawer(draw_balls)
+            pg.display.update()
+            for event in pg.event.get():
+                M_Eng.Event_Holder(event, balls, draw_balls, SR.TimerBull)
+                if event.type == pg.QUIT:
+                    game_running = False
+                    running = False
 
 pg.mixer.music.stop()
--- source_code/Engine/menu/start_menu.py
+++ source_code/Engine/menu/start_menu.py
@@ -14,15 +14,16 @@ class Button:
         if abs(self.x - event.pos[0]) < self.xsize and abs(self.y - event.pos[1]) < self.ysize:
             return True
 
-    def write_text_on_button(self):
-        myfont = pg.font.SysFont("monospace", 10)
-        text = myfont.render(str(self.text), 1, (0, 0, 0))
-        pg.draw.screen.blit(text, (self.x - self.xsize, self.y - self.ysize))
+    def write_text_on_button(self,screen):
+        myfont = pg.font.SysFont("monospace", 30)
+        text = myfont.render(str(self.text), 1, (255, 255, 255))
+        screen.blit(text, (self.x - self.xsize, self.y - self.ysize))
 
 
 # start = 0
+menu = True
 trek_choice = False
-trek_value = 0
+trek_number = 0
 
 
 class PlayButton(Button):
@@ -36,24 +37,20 @@ class PlayButton(Button):
 
 class TrekButton(Button):
     def __init__(self, *args, trek_number):
-        super().__init__(*args, trek_number)
+        super().__init__(*args)
         self.trek_number = trek_number
 
     def get_trek_number(self):
         return self.trek_number
 
 
-class QuitButton(Button):
+'''class QuitButton(Button):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def quit(self):
-        pg.QUIT
-
-
-class QuitButton(Button):
+class BackToMenuButton(Button):
     def __init__(self, *args):
         super().__init__(*args)
+'''
 
-    def quit(self):
-        pg.QUIT
+
