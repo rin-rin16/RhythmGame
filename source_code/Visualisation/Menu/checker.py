29d8e7c68733d6ce84ccf074d1e23e32c847fa29
gitpat-ea <pateshman.ea@phystech.edu>
2022-11-28 16:29:39 +0300

    try to make a good clicking pause menu

--- source_code/Visualisation/Menu/checker.py
+++ source_code/Visualisation/Menu/checker.py
@@ -8,6 +8,7 @@ import Vis_Pause_Menu as pm
 pg.init()
 
 screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
 
 finished = False
 
@@ -18,8 +19,22 @@ start_menu = sm.DrawAMenuButton(screen)
 pause_menu = pm.DrawAMenuButton(screen)
 
 while not finished:
-    pause_menu.all_menu_drawer_unpressed()
-    pg.display.update()
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:
+            screen.fill((0, 0, 0))
+            pause_menu.all_menu_drawer_pressed()
+            pg.display.update()
+            clock.tick(0.5)
+            screen.fill((0, 0, 0))
+            pause_menu.all_menu_drawer_unpressed()
+            pg.display.update()
+        else:
+            screen.fill((0, 0, 0))
+            pause_menu.all_menu_drawer_unpressed()
+            pg.display.update()
+
+
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
