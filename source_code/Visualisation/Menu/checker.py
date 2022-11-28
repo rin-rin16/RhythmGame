1c3e0ae8e9424741a0ecb0aff2c3690cef638ac5
gitpat-ea <pateshman.ea@phystech.edu>
2022-11-28 15:36:50 +0300

    edited name

--- source_code/Visualisation/Menu/checker.py
+++ source_code/Visualisation/Menu/checker.py
@@ -1,9 +1,9 @@
 import pygame as pg
-import pygame.key
+from source_code.Visualisation.Background import Background as bg
 
 import Vis_Choose_a_song_Menu as csm
 import Vis_Start_Menu as sm
-import Vis_Pause_Meny as pm
+import Vis_Pause_Menu as pm
 
 pg.init()
 
@@ -18,16 +18,10 @@ start_menu = sm.DrawAMenuButton(screen)
 pause_menu = pm.DrawAMenuButton(screen)
 
 while not finished:
-    for event in pg.event.get():
-        if pg.key.get_pressed()[pg.K_SPACE]:
-            pause_menu.all_menu_drawer_pressed()
-            pg.display.update()
-        else:
-            pause_menu.all_menu_drawer_unpressed()
-            pg.display.update()
+    pause_menu.all_menu_drawer_unpressed()
+    pg.display.update()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
 
-print(pg.font.get_fonts())
 pg.quit()
