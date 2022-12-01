f0f4af3e2c0d9c6f5fce1f80216f9a40b3868e7c
SavoSyka <savostikov.artem@gmail.com>
2022-12-01 03:26:49 +0300

    drawing ang logic of menu buttons migrated form main to start_menu in self functions

--- source_code/Engine/menu/start_menu.py
+++ source_code/Engine/menu/start_menu.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from source_code.Engine import Mouse_Mode as M_Eng
+from source_code.Visualisation.Game import Text_Before_Game as TBG
+
 
 class Button:
 
@@ -14,7 +16,7 @@ class Button:
         if self.x < event.pos[0] < self.x + self.xsize and self.y < event.pos[1] < self.y + self.ysize:
             return True
 
-    def write_text_on_button(self,screen):
+    def write_text_on_button(self, screen):
         myfont = pg.font.SysFont("monospace", 30)
         text = myfont.render(str(self.text), 1, (255, 255, 255))
         screen.blit(text, (self.x, self.y))
@@ -43,13 +45,19 @@ class TrekButton(Button):
     def get_trek_number(self):
         return self.trek_number
 
-def make_the_buttons_great_again(running):
+
+def draw_menu_buttons():
+    global play_button
+    global quit_button
     M_Eng.screen.fill((0, 0, 0))
     play_button = PlayButton(100, 100, 50, 50, 'play_button')
     quit_button = Button(100, 200, 50, 50, 'quit_button')
     play_button.write_text_on_button(M_Eng.screen)
     quit_button.write_text_on_button(M_Eng.screen)
     pg.display.update()
+
+
+def logic_of_menu_buttons(play_button, quit_button, running, trek_choice, clock):
     for event in pg.event.get():
         if event.type == pg.MOUSEBUTTONDOWN:
             if play_button.is_click(event):
@@ -59,3 +67,34 @@ def make_the_buttons_great_again(running):
                 running.setter(False)
         elif event.type == pg.QUIT:
             running.setter(False)
+
+    if trek_choice:
+        choice_running = True
+        while choice_running:
+            M_Eng.screen.fill((0, 0, 0))
+            amount_of_buttons = 5
+            trek_button = [0] * amount_of_buttons
+            for i in range(amount_of_buttons):
+                trek_button[i] = TrekButton(100, i * 100 + 100, 50, 50, f'trek_button_{i + 1}',
+                                            trek_number=i + 1)
+                trek_button[i].write_text_on_button(M_Eng.screen)
+            back_to_menu = Button(500, 100, 50, 50, 'Back')
+            back_to_menu.write_text_on_button(M_Eng.screen)
+            pg.display.update()
+
+            for event in pg.event.get():
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    for i in range(amount_of_buttons):
+                        if trek_button[i].is_click(event):
+                            clock.tick(1)
+                            for j in range(0, 5):
+                                TBG.countdown(j)
+                            trek_number = i + 1
+                            choice_running = False
+                    if back_to_menu.is_click(event):
+                        menu_running = True
+                        choice_running = False
+                        trek_choice = False
+                elif event.type == pg.QUIT:
+                    choice_running = False
+                    running.setter(False)
