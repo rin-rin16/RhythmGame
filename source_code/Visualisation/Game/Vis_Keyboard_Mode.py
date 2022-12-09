d2f27a2229bbc52c20ed4348bbed45e83c8e3737
gitpat-ea <pateshman.ea@phystech.edu>
2022-12-09 13:18:12 +0300

    PEP fixed

--- source_code/Visualisation/Game/Vis_Keyboard_Mode.py
+++ source_code/Visualisation/Game/Vis_Keyboard_Mode.py
@@ -8,14 +8,15 @@ black = (0, 0, 0)
 blue = (0, 0, 255)
 orange = (255, 180, 0)
 
+
 class DrawableArrow:
     """
-    draws an arrow in directplace
+    draws an arrow in direct place
     """
     def __init__(self, surface, x, y, scale, condition='normal'):
         """
 
-        :param surface: surface, where arrow must be drawed
+        :param surface: surface, where arrow must be drawn
         :param x: x position of arrow's center
         :param y: y position of arrow's center
         :param scale: scale of an arrow
@@ -26,7 +27,7 @@ class DrawableArrow:
         self.x = x
         self.y = y
         self.scale = scale
-        self.condition = 'normal'
+        self.condition = condition
 
     def set_condition(self, condition):
         self.condition = condition
@@ -38,6 +39,8 @@ class DrawableArrowUp(DrawableArrow):
         super().__init__(*args)
 
     def draw_up_arrow(self):
+        color_arrow = None
+        color_bar = None
         if self.condition == 'normal':
             color_arrow = white
             color_bar = yellow
@@ -47,9 +50,13 @@ class DrawableArrowUp(DrawableArrow):
         elif self.condition == 'bad':
             color_arrow = red
             color_bar = red
-        pg.draw.rect(self.surface, color_arrow, (self.x-2.5*self.scale, self.y, 5*self.scale, 8*self.scale), (self.scale)//3)
-        pg.draw.polygon(self.surface, color_arrow, ((self.x-6*self.scale, self.y), (self.x, self.y-8*self.scale), (self.x+6*self.scale, self.y)), (self.scale)//3)
-        pg.draw.rect(self.surface, color_bar, (self.x-0.3*self.scale, self.y-(44/6)*self.scale, 0.6*self.scale, (44/6+8)*self.scale))
+        pg.draw.rect(self.surface, color_arrow, (self.x-2.5*self.scale, self.y,
+                                                 5*self.scale,          8*self.scale), self.scale//3)
+        pg.draw.polygon(self.surface, color_arrow, ((self.x-6*self.scale, self.y),
+                                                    (self.x, self.y-8*self.scale),
+                                                    (self.x+6*self.scale, self.y)), self.scale//3)
+        pg.draw.rect(self.surface, color_bar, (self.x-0.3*self.scale, self.y-(44/6)*self.scale,
+                                               0.6*self.scale,        (44/6+8)*self.scale))
 
 
 class DrawableArrowDown(DrawableArrow):
@@ -58,6 +65,8 @@ class DrawableArrowDown(DrawableArrow):
         super().__init__(*args)
 
     def draw_down_arrow(self):
+        color_arrow = None
+        color_bar = None
         if self.condition == 'normal':
             color_arrow = white
             color_bar = yellow
@@ -67,10 +76,13 @@ class DrawableArrowDown(DrawableArrow):
         elif self.condition == 'bad':
             color_arrow = red
             color_bar = red
-        pg.draw.rect(self.surface, color_arrow, (self.x - 2.5 * self.scale, self.y - 8 * self.scale, 5 * self.scale, 8 * self.scale), (self.scale) // 3)
-        pg.draw.polygon(self.surface, color_arrow, ((self.x - 6 * self.scale, self.y), (self.x, self.y + 8 * self.scale), (self.x + 6 * self.scale, self.y)), (self.scale) // 3)
-        pg.draw.rect(self.surface, color_bar, (self.x - 0.3 * self.scale, self.y - 8 * self.scale, 0.6 * self.scale, (44 / 6 + 8) * self.scale))
-
+        pg.draw.rect(self.surface, color_arrow, (self.x - 2.5 * self.scale, self.y - 8 * self.scale,
+                                                 5 * self.scale,            8 * self.scale), self.scale // 3)
+        pg.draw.polygon(self.surface, color_arrow, ((self.x - 6 * self.scale, self.y),
+                                                    (self.x, self.y + 8 * self.scale),
+                                                    (self.x + 6 * self.scale, self.y)), self.scale // 3)
+        pg.draw.rect(self.surface, color_bar, (self.x - 0.3 * self.scale, self.y - 8 * self.scale,
+                                               0.6 * self.scale,          (44 / 6 + 8) * self.scale))
 
 
 class DrawableArrowRight(DrawableArrow):
@@ -79,6 +91,8 @@ class DrawableArrowRight(DrawableArrow):
         super().__init__(*args)
 
     def draw_right_arrow(self):
+        color_arrow = None
+        color_bar = None
         if self.condition == 'normal':
             color_arrow = white
             color_bar = yellow
@@ -88,9 +102,13 @@ class DrawableArrowRight(DrawableArrow):
         elif self.condition == 'bad':
             color_arrow = red
             color_bar = red
-        pg.draw.rect(self.surface, color_arrow, (self.x - 8 * self.scale, self.y - 2.5 * self.scale, 8 * self.scale, 5 * self.scale), (self.scale) // 3)
-        pg.draw.polygon(self.surface, color_arrow, ((self.x, self.y - 6 * self.scale), (self.x + 8 * self.scale, self.y), (self.x, self.y + 6 * self.scale)), (self.scale) // 3)
-        pg.draw.rect(self.surface, color_bar, (self.x - 8 * self.scale, self.y - 0.3 * self.scale, (44 / 6 + 8) * self.scale, 0.6 * self.scale))
+        pg.draw.rect(self.surface, color_arrow, (self.x - 8 * self.scale, self.y - 2.5 * self.scale,
+                                                 8 * self.scale,          5 * self.scale), self.scale // 3)
+        pg.draw.polygon(self.surface, color_arrow, ((self.x, self.y - 6 * self.scale),
+                                                    (self.x + 8 * self.scale, self.y),
+                                                    (self.x, self.y + 6 * self.scale)), self.scale // 3)
+        pg.draw.rect(self.surface, color_bar, (self.x - 8 * self.scale,   self.y - 0.3 * self.scale,
+                                               (44 / 6 + 8) * self.scale, 0.6 * self.scale))
 
 
 class DrawableArrowLeft(DrawableArrow):
@@ -99,6 +117,8 @@ class DrawableArrowLeft(DrawableArrow):
         super().__init__(*args)
 
     def draw_left_arrow(self):
+        color_arrow = None
+        color_bar = None
         if self.condition == 'normal':
             color_arrow = white
             color_bar = yellow
@@ -108,6 +128,10 @@ class DrawableArrowLeft(DrawableArrow):
         elif self.condition == 'bad':
             color_arrow = red
             color_bar = red
-        pg.draw.rect(self.surface, color_arrow, (self.x, self.y - 2.5 * self.scale, 8 * self.scale, 5 * self.scale), (self.scale) // 3)
-        pg.draw.polygon(self.surface, color_arrow, ((self.x, self.y - 6 * self.scale), (self.x - 8 * self.scale, self.y), (self.x, self.y + 6 * self.scale)), (self.scale) // 3)
-        pg.draw.rect(self.surface, color_bar, (self.x - (44/6) * self.scale, self.y - 0.3 * self.scale, (44 / 6 + 8) * self.scale, 0.6 * self.scale))
+        pg.draw.rect(self.surface, color_arrow, (self.x,         self.y - 2.5 * self.scale,
+                                                 8 * self.scale, 5 * self.scale), self.scale // 3)
+        pg.draw.polygon(self.surface, color_arrow, ((self.x, self.y - 6 * self.scale),
+                                                    (self.x - 8 * self.scale, self.y),
+                                                    (self.x, self.y + 6 * self.scale)), self.scale // 3)
+        pg.draw.rect(self.surface, color_bar, (self.x - (44/6) * self.scale, self.y - 0.3 * self.scale,
+                                               (44 / 6 + 8) * self.scale,    0.6 * self.scale))
