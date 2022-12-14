b5e662fa79a05b617070ad34fad01db3b55c6bb2
gitpat-ea <pateshman.ea@phystech.edu>
2022-12-14 11:02:44 +0300

    more colorful text before game and word back in choose song menu

--- source_code/Visualisation/Game/Text_Before_Game.py
+++ source_code/Visualisation/Game/Text_Before_Game.py
@@ -1,7 +1,21 @@
 import pygame as pg
 from source_code.Engine import Mouse_Mode as M_Eng
+import os
+
+black = (0, 0, 0)
+pink = (255, 150, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+yellow = (255, 255, 6)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+orange = (255, 180, 0)
+dark_grey = (50, 50, 50)
+light_grey = (90, 90, 90)
 
 clock = pg.time.Clock()
+
+colors = [red, yellow, green]
 
 
 
@@ -9,22 +23,22 @@ def countdown(number):
     if number == 4:
         clock.tick(1)
         M_Eng.screen.fill((0, 0, 0))
-        myfont = pg.font.SysFont("monospace", 30)
-        text = myfont.render('Get Ready', 1, (255, 255, 255))
-        M_Eng.screen.blit(text, (500, 300))
+        myfont = pg.font.Font(os.path.join(os.getcwd(), "source_code", "Visualisation", "Menu", 'Sunset Club Free Trial.ttf'), 100)
+        text = myfont.render('Get Ready', 1, pink)
+        M_Eng.screen.blit(text, (450, 300))
         pg.display.update()
         clock.tick(1)
     elif number == 0:
         M_Eng.screen.fill((0, 0, 0))
-        myfont = pg.font.SysFont("monospace", 30)
+        myfont = pg.font.Font(os.path.join(os.getcwd(), "source_code", "Visualisation", "Menu", 'Sunset Club Free Trial.ttf'), 100)
         text = myfont.render('GO!', 1, (255, 255, 255))
-        M_Eng.screen.blit(text, (500, 300))
+        M_Eng.screen.blit(text, (550, 300))
         pg.display.update()
         clock.tick(1)
     else:
         M_Eng.screen.fill((0, 0, 0))
-        myfont = pg.font.SysFont("monospace", 30)
-        text = myfont.render(f'{number}', 1, (255, 255, 255))
-        M_Eng.screen.blit(text, (500, 300))
+        myfont = pg.font.Font(os.path.join(os.getcwd(), "source_code", "Visualisation", "Menu", 'Sunset Club Free Trial.ttf'), 100)
+        text = myfont.render(f'{number}!', 1, colors[number-1])
+        M_Eng.screen.blit(text, (600, 300))
         pg.display.update()
         clock.tick(1)
