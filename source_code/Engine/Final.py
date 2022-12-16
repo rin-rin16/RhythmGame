20d4ef998871174c02030e138332794cfa600284
SavoSyka <savostikov.artem@gmail.com>
2022-12-16 06:38:04 +0300

    final menu logic absolutely done

--- source_code/Engine/Final.py
+++ source_code/Engine/Final.py
@@ -2,7 +2,7 @@ from source_code.Classes import Classes as CL
 import pygame as pg
 
 
-def logic(final_running,running,pressing,screen,mode_type,mode_choice,trek_choice,play_quit_menu):
+def logic(final_running,running,pressing,screen,mode_type,mode_choice,trek_choice,play_quit_menu,trek_number):
     if final_running.getter():
         screen.fill((0, 0, 0))
         go_to_menu = CL.Button(480, 200, 320, 180, 'Go_to_menu')
@@ -19,11 +19,13 @@ def logic(final_running,running,pressing,screen,mode_type,mode_choice,trek_choic
                         running.setter(False)
                     elif go_to_menu.is_click(event):
                         mode_type.setter(0)
-                        mode_choice.setter(False)
+                        mode_choice.setter(True)
                         trek_choice.setter(0)
+                        pressing.setter('none')
                         play_quit_menu.setter(True)
                         final_running.setter(False)
+                        trek_number.setter(0)
+
                 elif event.type == pg.QUIT:
                     running.setter(False)
-                    final_running = False
-
+                    final_running.setter(False)
