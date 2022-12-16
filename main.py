0bcdf06151a5d9aabd8d86eb287441576a3d739c
SavoSyka <savostikov.artem@gmail.com>
2022-12-16 04:55:57 +0300

    some classes migrated in need place

--- main.py
+++ main.py
@@ -30,9 +30,11 @@ ker_arr_list, lad_arr_list, pht_arr_list, wyn_arr_list, daa_arr_list = K_Eng.arr
 mode_type = CL.NumVariables()
 mode_choise = CL.BullVariables(False)
 play_quit_menu = CL.BullVariables(True)
+ending = CL.NumVariables(True)
+
 while running.getter():
     menu_running = True
-    start_menu.logic_of_menu_buttons(running, start_menu.trek_choice, clock, pressing_start, pressing_quit, mode_type, mode_choise, play_quit_menu,pressing)
+    start_menu.logic_of_menu_buttons(running, start_menu.trek_choice, clock, mode_type, mode_choise, ending, play_quit_menu,pressing)
     CL.start_time.setter(time.time())
 
     SR.Track_1_Player.music_player(CL.start_time, 60 / sb.K_K[0], sb.K_K[1], 0.1, 0.15, draw_balls, balls, running,
@@ -57,4 +59,4 @@ while running.getter():
     KR.Track_10_Player.music_player(CL.start_time, 60 / sb.D_A[0], sb.D_A[1], 0.1, 0.15, daa_arr_list, running,
                                    start_menu.trek_number.getter(), tcr.l_D_A)
 
-pg.mixer.music.stop()#
+pg.mixer.music.stop()
